b0ab77de24ed27c2c6a37cd0f2447675565fd4cd
yann-catlinn <y.catrilelbun@alumnos.duoc.cl>
2022-03-11 20:53:02 -0300

    links added and smooth scroll effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,22 @@ $(function() {
         }
     });
 });
+
+$(document).ready(function() {
+    $("a").click(function() {
+        var gato = this.hash;
+
+        $("html, body").animate({
+                scrollTop: $(gato).offset().top - 100,
+            },
+            800
+        );
+    });
+});
+
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
